358496148ce13722f426c97bdebe584e2908e644
ManoMax <gabrielmaxcontato@gmail.com>
2022-01-17 11:31:51 -0300

    refactor: changing dataFiliação for dataAfiliação

--- src/model/User.js
+++ src/model/User.js
@@ -48,7 +48,7 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
-    dataFiliacao: {
+    dataAfiliacao: {
         type: Date,
         required: true
     },
