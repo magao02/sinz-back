b489859a71a20d783870b637c3a4bbf43ffb6d43
ManoMax <gabrielmaxcontato@gmail.com>
2022-01-25 15:11:14 -0300

    fix: changing empty string to number zero

--- src/model/User.js
+++ src/model/User.js
@@ -92,62 +92,62 @@ const UserSchema = new Schema({
         janeiro: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         fevereiro: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         marco: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         abril: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         maio: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         junho: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         julho: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         agosto: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         setembro: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         outubro: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         novembro: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         },
         dezembro: {
             type: Number,
             required: false,
-            default: ""
+            default: 0
         }
     },
     endereco: {
