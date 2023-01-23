81db2173e689ca85c50063fcffc80b9dc721aacc
Samuel Lucas <samuellucasvmcontato@gmail.com>
2023-01-23 17:04:10 -0300

    fix: adding 0 verification to impostos

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -968,8 +968,6 @@ const UserController = {
 
       antigoImposto = antigoImposto[0];
 
-      console.log(antigoImposto, impostoDeRenda);
-
       const novoImposto = await Imposto.updateOne(
         { idUser: user.id, ano: +req.params.ano },
         {
@@ -977,73 +975,85 @@ const UserController = {
             janeiro:
               impostoDeRenda.janeiro !== undefined &&
               impostoDeRenda.janeiro !== null &&
-              impostoDeRenda.janeiro !== ""
+              impostoDeRenda.janeiro !== "" &&
+              impostoDeRenda.janeiro !== 0
                 ? impostoDeRenda.janeiro
                 : antigoImposto.janeiro,
             fevereiro:
               impostoDeRenda.fevereiro !== undefined &&
               impostoDeRenda.fevereiro !== null &&
-              impostoDeRenda.fevereiro !== ''
-                ? impostoDeRenda.fevereiro
+              impostoDeRenda.fevereiro !== '' &&
+              impostoDeRenda.fevereiro !== 0
+              ? impostoDeRenda.fevereiro
                 : antigoImposto.fevereiro,
             marco:
               impostoDeRenda.marco !== undefined &&
               impostoDeRenda.marco !== null &&
-              impostoDeRenda.marco !== ""
-                ? impostoDeRenda.marco
+              impostoDeRenda.marco !== "" &&
+              impostoDeRenda.marco !== 0
+              ? impostoDeRenda.marco
                 : antigoImposto.marco,
             abril:
               impostoDeRenda.abril !== undefined &&
               impostoDeRenda.abril !== null &&
-              impostoDeRenda.abril !== ""
-                ? impostoDeRenda.abril
+              impostoDeRenda.abril !== "" &&
+              impostoDeRenda.abril !== 0
+              ? impostoDeRenda.abril
                 : antigoImposto.abril,
             maio:
               impostoDeRenda.maio !== undefined &&
               impostoDeRenda.maio !== null &&
-              impostoDeRenda.maio !== ""
-                ? impostoDeRenda.maio
+              impostoDeRenda.maio !== "" &&
+              impostoDeRenda.maio !== 0
+              ? impostoDeRenda.maio
                 : antigoImposto.maio,
             junho:
               impostoDeRenda.junho !== undefined &&
               impostoDeRenda.junho !== null &&
-              impostoDeRenda.junho !== ""
-                ? impostoDeRenda.junho
+              impostoDeRenda.junho !== "" &&
+              impostoDeRenda.junho !== 0
+              ? impostoDeRenda.junho
                 : antigoImposto.junho,
             julho:
               impostoDeRenda.julho !== undefined &&
               impostoDeRenda.julho !== null &&
-              impostoDeRenda.julho !== ""
-                ? impostoDeRenda.julho
+              impostoDeRenda.julho !== "" &&
+              impostoDeRenda.julho !== 0
+              ? impostoDeRenda.julho
                 : antigoImposto.julho,
             agosto:
               impostoDeRenda.agosto !== undefined &&
               impostoDeRenda.agosto !== null &&
-              impostoDeRenda.agosto !== ""
-                ? impostoDeRenda.agosto
+              impostoDeRenda.agosto !== "" &&
+              impostoDeRenda.agosto !== 0
+              ? impostoDeRenda.agosto
                 : antigoImposto.agosto,
             setembro:
               impostoDeRenda.setembro !== undefined &&
               impostoDeRenda.setembro !== null &&
-              impostoDeRenda.setembro !== ""
-                ? impostoDeRenda.setembro
+              impostoDeRenda.setembro !== "" &&
+              impostoDeRenda.setembro !== 0
+              ? impostoDeRenda.setembro
                 : antigoImposto.setembro,
             outubro:
               impostoDeRenda.outubro !== undefined &&
               impostoDeRenda.outubro !== null &&
-              impostoDeRenda.outubro !== ""
-                ? impostoDeRenda.outubro
+              impostoDeRenda.outubro !== "" &&
+              impostoDeRenda.outubro !== 0
+              ? impostoDeRenda.outubro
                 : antigoImposto.outubro,
             novembro:
               impostoDeRenda.novembro !== undefined &&
               impostoDeRenda.novembro !== null &&
-              impostoDeRenda.novembro !== ""
-                ? impostoDeRenda.novembro
+              impostoDeRenda.novembro !== "" &&
+              impostoDeRenda.novembro !== 0
+              ? impostoDeRenda.novembro
                 : antigoImposto.novembro,
             dezembro:
               impostoDeRenda.dezembro !== undefined &&
               impostoDeRenda.dezembro !== null &&
-              impostoDeRenda.dezembro !== ""
+              impostoDeRenda.dezembro !== "" &&
+              impostoDeRenda.dezembro !== 0
                 ? impostoDeRenda.dezembro
                 : antigoImposto.dezembro,
           },
@@ -1077,7 +1087,7 @@ const UserController = {
           .status(HTTP_CODE_NOT_FOUND)
           .json({ message: "Dependente não encontrado." });
       }
-      let impostoDeRenda = req.body;
+      let impostoDeRenda = req.body.impostoDeRenda;
       let impostos = await Imposto.find({ idUser: dep._id });
 
 
@@ -1104,73 +1114,85 @@ const UserController = {
             janeiro:
               impostoDeRenda.janeiro !== undefined &&
               impostoDeRenda.janeiro !== null &&
-              impostoDeRenda.janeiro !== ""
+              impostoDeRenda.janeiro !== "" &&
+              impostoDeRenda.janeiro !== 0 
                 ? impostoDeRenda.janeiro
                 : antigoImposto.janeiro,
             fevereiro:
               impostoDeRenda.fevereiro !== undefined &&
               impostoDeRenda.fevereiro !== null &&
-              impostoDeRenda.fevereiro !== ""
+              impostoDeRenda.fevereiro !== "" &&
+              impostoDeRenda.fevereiro !== 0
                 ? impostoDeRenda.fevereiro
                 : antigoImposto.fevereiro,
             marco:
               impostoDeRenda.marco !== undefined &&
               impostoDeRenda.marco !== null &&
-              impostoDeRenda.marco !== ""
+              impostoDeRenda.marco !== "" &&
+              impostoDeRenda.marco !== 0
                 ? impostoDeRenda.marco
                 : antigoImposto.marco,
             abril:
               impostoDeRenda.abril !== undefined &&
               impostoDeRenda.abril !== null &&
-              impostoDeRenda.abril !== ""
+              impostoDeRenda.abril !== "" &&
+              impostoDeRenda.abril !== 0
                 ? impostoDeRenda.abril
                 : antigoImposto.abril,
             maio:
               impostoDeRenda.maio !== undefined &&
               impostoDeRenda.maio !== null &&
-              impostoDeRenda.maio !== ""
+              impostoDeRenda.maio !== "" &&
+              impostoDeRenda.maio !== 0
                 ? impostoDeRenda.maio
                 : antigoImposto.maio,
             junho:
               impostoDeRenda.junho !== undefined &&
               impostoDeRenda.junho !== null &&
-              impostoDeRenda.junho !== ""
+              impostoDeRenda.junho !== "" &&
+              impostoDeRenda.junho !== 0
                 ? impostoDeRenda.junho
                 : antigoImposto.junho,
             julho:
               impostoDeRenda.julho !== undefined &&
               impostoDeRenda.julho !== null &&
-              impostoDeRenda.julho !== ""
+              impostoDeRenda.julho !== "" &&
+              impostoDeRenda.julho !== 0
                 ? impostoDeRenda.julho
                 : antigoImposto.julho,
             agosto:
               impostoDeRenda.agosto !== undefined &&
               impostoDeRenda.agosto !== null &&
-              impostoDeRenda.agosto !== ""
+              impostoDeRenda.agosto !== "" &&
+              impostoDeRenda.agosto !== 0
                 ? impostoDeRenda.agosto
                 : antigoImposto.agosto,
             setembro:
               impostoDeRenda.setembro !== undefined &&
               impostoDeRenda.setembro !== null &&
-              impostoDeRenda.setembro !== ""
+              impostoDeRenda.setembro !== "" &&
+              impostoDeRenda.setembro !== 0
                 ? impostoDeRenda.setembro
                 : antigoImposto.setembro,
             outubro:
               impostoDeRenda.outubro !== undefined &&
               impostoDeRenda.outubro !== null &&
-              impostoDeRenda.outubro !== ""
+              impostoDeRenda.outubro !== "" &&
+              impostoDeRenda.outubro !== 0
                 ? impostoDeRenda.outubro
                 : antigoImposto.outubro,
             novembro:
               impostoDeRenda.novembro !== undefined &&
               impostoDeRenda.novembro !== null &&
-              impostoDeRenda.novembro !== ""
+              impostoDeRenda.novembro !== "" &&
+              impostoDeRenda.novembro !== 0
                 ? impostoDeRenda.novembro
                 : antigoImposto.novembro,
             dezembro:
               impostoDeRenda.dezembro !== undefined &&
               impostoDeRenda.dezembro !== null &&
-              impostoDeRenda.dezembro !== ""
+              impostoDeRenda.dezembro !== "" &&
+              impostoDeRenda.dezembro !== 0
                 ? impostoDeRenda.dezembro
                 : antigoImposto.dezembro,
           },
