5ea7f091139c04ec2de4751906a81aacd880bdba
PedroCost4 <pedro.costacdc@gmail.com>
2023-07-03 19:59:34 -0300

    feat(Dependant): added update dependant

--- src/controller/dependenteController.js
+++ src/controller/dependenteController.js
@@ -1,6 +1,7 @@
 const signUpDep = require("../service/dependenteFunctions/signUpDep");
 const getDependents = require("../service/dependenteFunctions/getDependents");
 const deleteDep = require("../service/dependenteFunctions/deleteDep");
+const updateDep = require("../service/dependenteFunctions/updateDep")
 
 module.exports = {
   async signUpDep(req, res) {
@@ -9,8 +10,10 @@ module.exports = {
   async getDependents(req, res) {
     return getDependents(req, res);
   },
-
   async deleteDep(req, res) {
     return deleteDep(req, res);
   },
+  async updateDep(req,res) {
+    return updateDep(req,res);
+  }
 };
