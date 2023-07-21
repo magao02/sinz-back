a0ee53b11b1413fa9cbc59a48580ce496aef4bbf
PedroCost4 <pedro.costacdc@gmail.com>
2023-07-21 12:15:29 -0300

    feat: apartment model

---
+++ src/model/Apartment.js
@@ -0,0 +1,89 @@
+const { Schema, model } = require("mongoose");
+
+const ApartmentSchema = new Schema({
+  titulo: {
+    type: String,
+    required: true,
+  },
+  urlApt: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  endereco: {
+    type: String,
+    required: true,
+  },
+  descricao: {
+    type: String,
+    required: false,
+  },
+  tipo: {
+    type: String,
+    required: true,
+  },
+  andar: {
+    type: Number,
+    required: true,
+  },
+  suite: {
+    type: Boolean,
+    required: true,
+  },
+  wifi: {
+    type: Boolean,
+    required: true,
+  },
+  animais: {
+    type: Boolean,
+    required: true,
+  },
+  diaria: {
+    type: Number,
+    required: true,
+  },
+  images: {
+    type: [String],
+    required: false,
+    default: [],
+  },
+  camas: [
+    {
+      tipo: {
+        type: String,
+        required: false,
+      },
+      quantidade: {
+        type: Number,
+        required: false,
+      },
+    },
+  ],
+  locaisArredores: {
+    type: [String],
+    required: false,
+    default: [],
+  },
+  itens: {
+    type: [String],
+    required: false,
+    default: [],
+  },
+  reservas: {
+    type: [String],
+    required: false,
+    default: [],
+  },
+  areasComuns: {
+    type: [String],
+    required: false,
+    default: [],
+  },
+  regrasConvivencia: {
+    type: [String],
+    required: false,
+    default: [],
+  },
+});
+
+module.exports = model("Apartment", ApartmentSchema);
