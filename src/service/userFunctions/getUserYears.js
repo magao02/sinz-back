e86820d66db90194c8a9ad6406511bf6f86cbc4e
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-08-21 14:39:27 -0300

    fix: fix error in getUserYears

--- src/service/userFunctions/getUserYears.js
+++ src/service/userFunctions/getUserYears.js
@@ -1,6 +1,6 @@
 const User = require("../../model/User");
 const Imposto = require("../../model/Imposto");
-const { HTTP_CODE_NOT_FOUND } = require("../../utils/httpStatus");
+const { HTTP_CODE_NOT_FOUND, HTTP_CODE_OK } = require("../../utils/httpStatus");
 
 async function getUserYears(req, res) {
   const urlUser = req.params.urlUser;
