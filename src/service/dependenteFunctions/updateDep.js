dbd1a25d6b7ecbf52bb1ddf44242c9098012b57e
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-08-23 19:12:23 -0300

    feat: make updateDep actually work

--- src/service/dependenteFunctions/updateDep.js
+++ src/service/dependenteFunctions/updateDep.js
@@ -1,28 +1,49 @@
 const Dependent = require("../../model/Dependent");
+const formataData = require("../../utils/dateFunctions");
 const {
   HTTP_CODE_OK,
   HTTP_CODE_UNAUTHORIZED,
   HTTP_CODE_NOT_FOUND,
+  HTTP_CODE_BAD_REQUEST,
 } = require("../../utils/httpStatus");
 
-const isBlank = (string) => {
-  return string !== undefined && string !== "";
-};
-
 const editDep = async (req, res) => {
-  if (req.user.admin) {
-    const urlDep = req.params.urlDep;
+  const urlDep = req.params.urlDep;
 
-    let dep = Dependent.findOne({ urlDep });
+  let dep = await Dependent.findOne({ urlDep });
 
-    if (!dep) {
+  if (!dep) {
+    return res
+      .status(HTTP_CODE_NOT_FOUND)
+      .json({ message: "Dependente não encontrado." });
+  }
+
+  if (req.user.admin || req.user._id.equals(dep.idAssociado)) {
+    let {
+      name,
+      nascimento,
+      cpf,
+      rg,
+      emissao,
+      parentesco,
+    } = req.body;
+
+    try {
+      if (nascimento !== undefined && nascimento !== "") {
+        nascimento = await formataData(nascimento);
+      }
+      if (emissao !== undefined && emissao !== "") {
+        emissao = await formataData(emissao);
+      }
+    } catch (err) {
       return res
-        .status(HTTP_CODE_NOT_FOUND)
-        .json({ message: "Dependente não encontrado." });
+        .status(HTTP_CODE_BAD_REQUEST)
+        .json("Data inserida incorretamente. Formato correto: dd/mm/aaaa");
     }
 
-    const data = req.body;
-    dep = await Dependant.findByIdAndUpdate(dep._id, data);
+    dep = await Dependent.findByIdAndUpdate(dep._id, {
+      name, nascimento, cpf, rg, emissao, parentesco
+    });
 
     return res
       .status(HTTP_CODE_OK)
