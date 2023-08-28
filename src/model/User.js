96e8105f5c58472d37d7aef3a18348f86ce3cba6
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-08-28 18:45:06 -0300

    feat: add telefone fixo

--- src/model/User.js
+++ src/model/User.js
@@ -28,6 +28,10 @@ const UserSchema = new Schema(
       type: String,
       required: false,
     },
+    telefoneFixo: {
+      type: String,
+      required: false,
+    },
     nascimento: {
       type: Date,
       required: false,
