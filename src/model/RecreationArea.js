3ddd0f2da520494f9b68a0b9ccdc26a5f4c1cd36
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-09-01 17:40:44 -0300

    feat: set recreation area photos

--- src/model/RecreationArea.js
+++ src/model/RecreationArea.js
@@ -81,16 +81,23 @@ const recreationAreaSchema = new Schema({
     required: false,
     default: [],
   },
-  imageUrl: {
-    type: [String],
-    required: false,
-    default: [],
-  },
   regrasConvivencia: {
     type: [String],
     required: false,
     default: [],
   },
+  pictures: [
+    {
+      key: {
+        type: String,
+        required: false
+      },
+      url: {
+        type: String,
+        required: false
+      }
+    },
+  ],
 });
 
 module.exports = model("RecreationArea", recreationAreaSchema);
