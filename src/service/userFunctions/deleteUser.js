77c2ee5a8420ba1f559af08566d3dd4a82bada4c
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-09-18 14:15:24 -0300

    feat: delete profile picture and reservations on user delete

--- src/service/userFunctions/deleteUser.js
+++ src/service/userFunctions/deleteUser.js
@@ -1,6 +1,9 @@
 const User = require("../../model/User");
 const Imposto = require("../../model/Imposto");
 const Dependent = require("../../model/Dependent");
+const Apartment = require("../../model/Apartment");
+const RecreationArea = require("../../model/RecreationArea");
+const S3Storage = require("../../utils/S3Storage");
 const {
   HTTP_CODE_OK,
   HTTP_CODE_UNAUTHORIZED,
@@ -20,8 +23,6 @@ async function deleteUser(req, res) {
 
     await Imposto.deleteMany({ idUser: user._id });
 
-    // FIXME: delete profile picture from S3?
-
     user = await User.deleteOne(user)
       .then(async function (deletedUser) {
         if (deletedUser) {
@@ -29,6 +30,16 @@ async function deleteUser(req, res) {
             await Dependent.findByIdAndDelete(user.dependentes[i]);
             await Imposto.deleteMany({ idUser: user.dependentes[i]._id });
           }
+          
+          // delete reservations by this user
+          await Apartment.updateMany({}, { $pull: { reservas: { idAssociado: user._id } } });
+          await RecreationArea.updateMany({}, { $pull: { reservas: { idAssociado: user._id } } });
+          
+          // delete this user's profile picture from S3
+          if (user.profilePic?.key) {
+            await S3Storage.deleteFile(user.profilePic.key);
+          }
+
           return res.status(HTTP_CODE_OK).json({
             message: `Usuário (${user.name}) deletado com sucesso.`,
           });
