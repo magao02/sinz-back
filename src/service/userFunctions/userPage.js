baef20ff7856e40ef8f263c668f455e7e83bcb94
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-10-23 21:07:45 -0300

    fix: send isPendingSignup on userPage

--- src/service/userFunctions/userPage.js
+++ src/service/userFunctions/userPage.js
@@ -60,6 +60,7 @@ async function userPage(req, res) {
     profilePic: getImageUrl(user.profilePic),
     admin: user.admin,
     universidade: user.universidade,
+    isPendingSignup: user.isPendingSignup,
   };
   return res.status(HTTP_CODE_OK).json(dataPage);
 }
